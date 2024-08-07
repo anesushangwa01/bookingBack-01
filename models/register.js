53a1e073bac1af2b0a2e47cd82cfffa1c6c0a181
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2024-08-07 14:00:42 +0200

    new

--- models/register.js
+++ models/register.js
@@ -11,3 +11,30 @@ const userSchema = new mongoose.Schema({
 const register = mongoose.model('register', userSchema);
 
 module.exports = register;
+
+
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+
+// const userSchema = new mongoose.Schema({
+//   name: { type: String, unique: true },
+//   email: { type: String, unique: true },
+//   password: String,
+//   favoriteMovie: String,
+//   favoriteCountry: String
+// });
+
+// // Middleware to encrypt sensitive fields
+// userSchema.pre('save', async function(next) {
+//   if (this.isModified('favoriteMovie')) {
+//     this.favoriteMovie = await bcrypt.hash(this.favoriteMovie, 10);
+//   }
+//   if (this.isModified('favoriteCountry')) {
+//     this.favoriteCountry = await bcrypt.hash(this.favoriteCountry, 10);
+//   }
+//   next();
+// });
+
+// const register = mongoose.model('register', userSchema);
+
+// module.exports = register;
